7c357b1a5a8f03fed533a1ac5b32a2a9826d5fe5
Pratyush Landekar <pratyushlandekar103@gmail.com>
2024-09-26 03:59:11 +0530

    feat: improved Auth State Indication

--- src/components/user/userbadge.jsx
+++ src/components/user/userbadge.jsx
@@ -1,17 +1,30 @@
-import React from "react";
+import React,{ useContext } from "react";
+import PropTypes from "prop-types";
+import Dropdown  from "react-bootstrap/Dropdown";
+import { AuthContext } from "../auth/AuthContext";
+import { Link } from "react-router-dom";
 
 const UserBadge = ({ profilePicture, fullName }) => {
+  const { logout } = useContext(AuthContext);
   return (
-    <div className="btn btn-warning dropdown">
-      <button className="dropdown-toggle">
-        <img src={profilePicture} alt="User Profile" />
-        <h5>{fullName}</h5>
-      </button>
-      <ul className="dropdown-menu">
-        <li><a href="" className="dropdown-item">Profile</a></li>
-      </ul>
-    </div>
+    <Dropdown>
+      <Dropdown.Toggle variant="primary" id="dropdown-basic" className="userBadge">
+        <img src={profilePicture} alt="User Profile" width="10%" className="rounded-circle mr-2"/>
+        <span>{fullName}</span>
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu>
+        <Dropdown.Item as={Link} to="/profile">Profile</Dropdown.Item>
+        <Dropdown.Item as={Link} to="/dashboard">Dashboard</Dropdown.Item>
+        <Dropdown.Item variant="danger" onClick={() => {if(confirm("Do you want to Logout?"))logout()}}>Logout</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
   );
+};
+
+UserBadge.propTypes = {
+  profilePicture: PropTypes.string.isRequired,
+  fullName: PropTypes.string.isRequired,
 };
 
 export { UserBadge };
