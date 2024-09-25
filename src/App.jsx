7af009ff208b0b29c990453071d2a2674f97fb20
Pratyush Landekar <pratyushlandekar103@gmail.com>
2024-09-26 03:55:03 +0530

    feat: dynamic themes, new dependecies

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,19 @@ import ErrorBoundary from "./components/ErrorBoundary";
 // App.jsx
 function App() {
   useEffect(() => {
-    document.documentElement.setAttribute("data-bs-theme", "dark");
+    const updateTheme = () => {
+      const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
+      const theme = prefersDarkScheme ? "dark" : "light";
+      document.documentElement.setAttribute("data-bs-theme", theme);
+    };
+
+    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+    updateTheme();
+    mediaQuery.addEventListener("change", updateTheme);
+
+    return () => {
+      mediaQuery.removeEventListener("change", updateTheme);
+    };
   }, []);
 
   return (
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import 'bootstrap';
 import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap/dist/js/bootstrap.js';
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
